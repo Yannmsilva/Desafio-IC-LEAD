a3bf69e81218cedc980c7622323cdf81015285d0
Yann Machado <yannmachadosilva@hotmail.com>
2019-05-04 19:27:35 -0300

    Criação do primeiro .cpp file e tentativa de fazer a compilação funcionar.

---
+++ modulation.cpp
@@ -0,0 +1,23 @@
+//Código referente a modulação do sinal sensor.log
+#include <iostream>
+#include <fstream>
+
+//A ideia é fazer com que essa função faça os cálculos e o executá-la no main
+void modulation()
+{
+
+}
+
+
+
+int main()
+{
+	ofstream arquivo;
+	arquivo.open("teste.log");
+	arquivo << "Teste de criação de arquivos.\n";
+	arquivo.close();
+
+}
+
+
+
